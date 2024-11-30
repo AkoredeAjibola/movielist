90e62c8a70b85afd57abea7ec0ad9964a0c447ba
Akorede Ajibola <ajibolaakorede2021@gmail.com>
2024-11-30 13:42:55 +0100

    ...

--- backend/routes/reminderRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { addReminder, getReminders } from "../controllers/reminder.controller";
-
-const router = Router();
-
-router.post("/", addReminder);
-router.get("/user/:userId", getReminders);
-
-export default router;
