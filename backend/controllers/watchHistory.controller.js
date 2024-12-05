54dd367df2fbf2052ac393bc1f7fa09437c45516
Akorede Ajibola <ajibolaakorede2021@gmail.com>
2024-12-06 00:42:42 +0100

    ...

--- backend/controllers/watchHistory.controller.js
+++ backend/controllers/watchHistory.controller.js
@@ -1,9 +1,5 @@
 import { User } from '../models/user.model.js';
 
-
-
-import { User } from '../models/user.model.js';
-
 export const markAsWatched = async (req, res) => {
   try {
     const { movieId, watched, movieTitle } = req.body;
