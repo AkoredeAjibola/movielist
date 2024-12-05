1a5ed79acf66495bbd47d8fff73a70856fcadf9b
Akorede Ajibola <ajibolaakorede2021@gmail.com>
2024-12-06 00:48:14 +0100

    ....

--- backend/routes/watchHistory.route.js
+++ backend/routes/watchHistory.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { addWatchHistory, getWatchHistory, deleteWatchHistory, markAsWatched } from '../controllers/watchHistory.controller.js';
+import { getWatchHistory, deleteWatchHistory, markAsWatched } from '../controllers/watchHistory.controller.js';
 import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.post('/add', protectRoute, addWatchHistory);
+
 router.get('/', protectRoute, getWatchHistory);
 router.delete('/:id', protectRoute, deleteWatchHistory);
 router.put("/watched", protectRoute, markAsWatched);
