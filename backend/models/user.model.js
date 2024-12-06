200820288f8beab0c05fbae2f1eb263b2c52ca09
Akorede Ajibola <ajibolaakorede2021@gmail.com>
2024-12-06 01:32:32 +0100

    ...

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -25,7 +25,7 @@ const userSchema = mongoose.Schema({
 	  watchHistory: [
 		{
 		  movieId: { type: String, required: true },
-		  movieTitle: { type: String, required: true },
+		  movieTitle: { type: String},
 		  watched: { type: Boolean, default: false },
 		  watchedAt: { type: Date, default: Date.now },
 		},
